ec278151c1ddd77dab55ccf58299873560163ff5
Victor Blasco Robles <vblasco@gamma23.aulas.gsyc.urjc.es>
2019-02-05 12:46:04 +0100

    Tablas de multiplicar

---
+++ tabla_multiplicar.py
@@ -0,0 +1,10 @@
+
+for num_tabla in range(1,11):
+	print ("Tabla del " + str(num_tabla))
+	print ("--------")
+	for nums in range(1,11):
+		resultado= num_tabla * nums
+		print(str(num_tabla) + " por " + str(nums) + " es " + str(resultado))
+
+
+
